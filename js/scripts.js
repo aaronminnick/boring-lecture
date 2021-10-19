27a5eb3d9ff6c2bfc756c47f963853706ecdea03
Aaron Minnick <abminnick@gmail.com>
2021-10-19 11:10:17 -0700

    add click to highlight a paragraph
    Co-authored-by: Albert Lee <leesga8@gmail.com>
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $("button#color-scheme").click(function() {
     $("body.light-scheme").toggleClass("dark-scheme");
   });
+  $("p").click(function() {
+    $(this).toggleClass("highlight");
+  });
 });
